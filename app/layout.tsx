c60017ee6a68835ce9ecb6f09051a25eb59ceedf
joncoronel <59675970+joncoronel@users.noreply.github.com>
2023-09-26 22:36:02 -0700

    testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,30 @@
+import "@mantine/core/styles.css";
 import React from "react";
-
-import "@mantine/core/styles/global.css";
-import "@mantine/core/styles/UnstyledButton.css";
-import "@mantine/core/styles/Button.css";
-import "@mantine/core/styles/Paper.css";
-import "@mantine/core/styles/Card.css";
-import "@mantine/core/styles/Text.css";
-import "@mantine/core/styles/Group.css";
-import "@mantine/core/styles/InlineInput.css";
-import "@mantine/core/styles/Input.css";
-import "@mantine/core/styles/ActionIcon.css";
-import "@mantine/core/styles/Tooltip.css";
-import "@mantine/core/styles/Stepper.css";
-import "@mantine/core/styles/Popover.css";
-import "@mantine/core/styles/Combobox.css";
-import "@mantine/core/styles/ScrollArea.css";
-import "@mantine/core/styles/Switch.css";
-import "@mantine/core/styles/ModalBase.css";
-import "@mantine/core/styles/Modal.css";
-
 import { MantineProvider, ColorSchemeScript } from "@mantine/core";
+
+
+export const metadata = {
+  title: "Mantine Next.js template",
+  description: "I am using Mantine with Next.js!",
+};
 
 export default function RootLayout({ children }: { children: any }) {
   return (
     <html lang="en">
       <head>
         <ColorSchemeScript defaultColorScheme="dark" />
+        <link rel="shortcut icon" href="/favicon.svg" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
       </head>
-      <body id="body">
-        <MantineProvider
-          defaultColorScheme="dark"
-          theme={{
-            fontFamily: "var(--font-best)",
-          }}
-        >
+      <body>
+        <MantineProvider defaultColorScheme="dark">
+   
           {children}
         </MantineProvider>
       </body>
     </html>
   );
 }
-
-export const runtime = "edge";
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
-import ClientComponent from "./components/ClientComponent";
-
 export default function HomePage() {
-  return (
-    <div>
-      Testing
-      <ClientComponent />
-    </div>
-  );
+  return <div>Home page</div>;
 }
 
-
+export const runtime = "edge";
